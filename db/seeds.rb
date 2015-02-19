c6e6db3c4b834d60b1253c2f9ebb311a9f012dd0
JT <jturino@gmail.com>
2015-02-18 16:45:01 -0800

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+connection = ActiveRecord::Base.connection
+
+%w[states districts zip_codes districts_zip_codes legislators].each do |table|
+  sql = File.read("db/seed_data/#{table}.sql")
+  statements = sql.split(/;$/)
+
+  ActiveRecord::Base.transaction do
+    statements.each do |statement|
+      connection.execute(statement)
+    end
+  end
+end
+
+%w[states districts zip_codes legislators].each do |table|
+  result = connection.execute("SELECT id FROM #{table} ORDER BY id DESC LIMIT 1")
+  connection.execute(
+    "ALTER SEQUENCE #{table}_id_seq RESTART WITH #{result.first['id'].to_i + 1}"
+  )
+end
