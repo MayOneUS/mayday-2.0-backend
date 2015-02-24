496c44b39bdb8fd403b37116a46699046bcec978
Rio Flores <rio517@gmail.com>
2015-02-24 15:03:33 -0500

    Minor: code formating

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,9 +1,8 @@
 class Person < ActiveRecord::Base
   has_many :locations
   has_one :current_location, -> { order 'created_at DESC' }, class_name: "Location"
-  has_many :disricts, :through => :locations
-  has_one :current_district, :through => :current_location, :source => 'district'
+  has_many :disricts, through: :locations
+  has_one :current_district, through: :current_location, source: 'district'
 
   validates :email, uniqueness: true
-
 end
