6a129994892eb97aa8b410435d5586d6082a50d0
Rio Flores <rio517@gmail.com>
2015-02-25 15:36:39 -0500

    Annotate: add annotations

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: locations
+#
+#  id            :integer          not null, primary key
+#  location_type :string
+#  address_1     :string
+#  address_2     :string
+#  city          :string
+#  state_id      :integer
+#  zip_code      :string
+#  person_id     :integer
+#  district_id   :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 class Location < ActiveRecord::Base
   belongs_to :person
   belongs_to :district
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: people
+#
+#  id         :integer          not null, primary key
+#  email      :string
+#  phone      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Person < ActiveRecord::Base
   has_one :location
   has_one :district, through: :location
