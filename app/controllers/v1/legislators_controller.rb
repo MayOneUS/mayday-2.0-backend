b99e9413ca7ebfa685d9d972f8be07d199b38d6d
JT <jturino@gmail.com>
2015-02-26 13:53:59 -0800

    move logic from view to LegislatorsController

--- app/controllers/v1/legislators_controller.rb
+++ app/controllers/v1/legislators_controller.rb
@@ -1,11 +1,14 @@
 class V1::LegislatorsController < V1::BaseController
   def index
-    @user = Person.find_or_initialize_by(email: params[:email])
-    if @user.save
-      @user.update_location( address: params[:address],
-                             city:    params[:city],
-                             state:   params[:state],
-                             zip:     params[:zip] )
+    user = Person.find_or_initialize_by(email: params[:email])
+    if user.save
+      user.update_location( address: params[:address],
+                            city:    params[:city],
+                            state:   params[:state],
+                            zip:     params[:zip] )
+
+      @target_legislators = user.target_legislators(json: true)
+      @address_required   = user.address_required?
     else
       @error = "user not found"
     end
