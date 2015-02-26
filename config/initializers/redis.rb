b98965d2aa863415c16689b0bab9dabde392e294
Andrew T. Baker <andrew.tork.baker@gmail.com>
2015-02-26 01:18:09 -0500

    Updating fig configuration to use REDIS and pass environment variables

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,7 @@
+if ENV["REDIS_URL"].present?
+  REDIS = Redis.new(:url => ENV['REDIS_URL'])
+end
+
 if ENV["REDISTOGO_URL"].present?
   uri = URI.parse(ENV["REDISTOGO_URL"])
   REDIS = Redis.new(:url => ENV['REDISTOGO_URL'])
