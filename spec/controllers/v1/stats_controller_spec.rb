a1fc2af22a8dddb87f5163861529e17d05e7f7d9
Rio Flores <rio517@gmail.com>
2015-03-01 21:24:34 -0500

    Minor: Code formating

--- spec/controllers/v1/stats_controller_spec.rb
+++ spec/controllers/v1/stats_controller_spec.rb
@@ -3,14 +3,15 @@ require 'rails_helper'
 describe V1::StatsController,  type: :controller do
 
   before(:all) do
-    @target_counts = {:supporter_count=>69087, :volunteer_count=>2151, :called_voters_count=>0, :reps_calls_count=>0, :house_supporters=>0, :senate_supporters=>0, :donations_total=>748608206, :donations_count=>65136}
+    @fake_counts = {supporter_count: 69087, volunteer_count: 2151, called_voters_count: 0, reps_calls_count: 0, house_supporters: 0, senate_supporters: 0, donations_total: 748608206, donations_count: 65136}
+
   end
 
   context 'with redis stored counts' do
     before(:all) do
      ExternalCountFetcher::REDIS_KEYS.each do |key|
       counter = Redis::Counter.new('external_count_fetcher:true:'+key.to_s)
-      counter.value = @target_counts[key]
+      counter.value = @fake_counts[key]
     end
     end
     after(:all){ Redis.new.flushdb }
@@ -23,7 +24,7 @@ describe V1::StatsController,  type: :controller do
       it "returns stored counts in json" do
         get :index
         expect_any_instance_of(ExternalCountFetcher).not_to receive(:fetch_empty_counts!)
-        expect(response.body).to eq @target_counts.to_json
+        expect(response.body).to eq @fake_counts.to_json
       end
     end
   end
