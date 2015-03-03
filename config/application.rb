fa74058e6e490b9d4de0a4d7431dc191b68e5f87
JT <jturino@gmail.com>
2015-03-02 19:28:54 -0800

    get rid of warning about `config.serve_static_assets`

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module Mayday
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.serve_static_assets = false
+    config.serve_static_files = false
 
     config.autoload_paths << Rails.root.join('lib')
 
