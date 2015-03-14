2579defa2ba5d3acfa6c1dafbad576393d40f752
Rio Flores <rio517@gmail.com>
2015-03-13 23:52:26 -0400

    BlogPosts: remove unneeded call, better naming

--- app/controllers/v1/blog_posts_controller.rb
+++ app/controllers/v1/blog_posts_controller.rb
@@ -1,15 +1,15 @@
 class V1::BlogPostsController < V1::BaseController
   def index
-    render json: BlogFetcher.feed(param: :recent, reset: parsed_reset)
+    render json: BlogFetcher.feed(param: :recent, reset: reset?)
   end
 
   def press_releases
-    render json: BlogFetcher.feed(param: :press_releases, reset: parsed_reset)
+    render json: BlogFetcher.feed(param: :press_releases, reset: reset?)
   end
 
   protected
 
-  def parsed_reset
-    params[:reset].presence == "1"
+  def reset?
+    params[:reset] == "1"
   end
 end
