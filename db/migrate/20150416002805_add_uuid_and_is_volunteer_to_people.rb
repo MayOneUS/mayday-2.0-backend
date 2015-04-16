6194eb32aebd422e11936bebbb0cba04b90366ec
JT <jturino@gmail.com>
2015-04-16 12:33:26 -0700

    fix migration

--- db/migrate/20150416002805_add_uuid_and_is_volunteer_to_people.rb
+++ db/migrate/20150416002805_add_uuid_and_is_volunteer_to_people.rb
@@ -3,10 +3,13 @@ class AddUuidAndIsVolunteerToPeople < ActiveRecord::Migration
     add_column :people, :uuid, :string
     add_column :people, :is_volunteer, :boolean
     add_index :people, :uuid, unique: true
-  end
-  def up
-    Person.unscoped.each do |person|
-      person.update(uuid: Person.new_uuid)
+
+    reversible do |change|
+      change.up do
+        Person.unscoped.each do |person|
+          person.update(uuid: Person.new_uuid)
+        end
+      end
     end
   end
 end
