a2952d58edcdd40b62ecb7dc27da85ae1297eb08
Rio Flores <rio517@gmail.com>
2015-04-17 17:37:03 -0400

    Seed updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,14 @@
 connection = ActiveRecord::Base.connection
 
+
+puts 'adding govt data'
 %w[states districts zip_codes districts_zip_codes legislators].each do |table|
+  puts "loading #{table}"
   connection.execute(IO.read("db/seed_data/#{table}.sql"))
 end
 
 %w[states districts zip_codes legislators].each do |table|
+  puts "loading #{table}"
   result = connection.execute("SELECT id FROM #{table} ORDER BY id DESC LIMIT 1")
   connection.execute(
     "ALTER SEQUENCE #{table}_id_seq RESTART WITH #{result.first['id'].to_i + 1}"
