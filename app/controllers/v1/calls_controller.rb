506f8b08b406f4bf02f736236be498efee789924
Rio Flores <rio517@gmail.com>
2015-04-23 15:09:03 -0400

    CallsController: use person class method to handle crud action

--- app/controllers/v1/calls_controller.rb
+++ app/controllers/v1/calls_controller.rb
@@ -1,7 +1,7 @@
 class V1::CallsController < V1::BaseController
 
   def create
-    person = Person.create_with(phone: params[:phone]).find_or_create_by(email: params[:email])
+    person = Person.create_or_update(params)
     twilio_call = Integration::Twilio.initiate_congress_calling(phone: params[:phone])
     call = person.calls.create(remote_id: twilio_call.sid)
     render json: {call_sid: call.remote_id}, status: 200
