7976a7d8511449b8f900b65cc190211bc838fd12
Rio Flores <rio517@gmail.com>
2015-04-23 16:46:55 -0400

    Fix calls paramaters error

--- app/controllers/v1/calls_controller.rb
+++ app/controllers/v1/calls_controller.rb
@@ -1,7 +1,7 @@
 class V1::CallsController < V1::BaseController
 
   def create
-    person = Person.create_or_update(params)
+    person = Person.create_or_update(params.permit(:uuid, :email, :phone))
     twilio_call = Integration::Twilio.initiate_congress_calling(phone: params[:phone])
     call = person.calls.create(remote_id: twilio_call.sid)
     render json: {call_sid: call.remote_id}, status: 200
