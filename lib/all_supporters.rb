8b94c7e5f5f1b9329ee5dcf21a97035d4549cc56
Rio Flores <rio517@gmail.com>
2015-04-28 14:35:11 -0400

    Add real total to AllSupporters

--- lib/all_supporters.rb
+++ lib/all_supporters.rb
@@ -1,6 +1,6 @@
 class Bill::AllSupporters
   attr_accessor :supporter_count, :cosponsor_count, :needed_cosponsor_count, :short_title, :chamber_size, :congressional_session
-
+  TOTAL_NEEDED = 219
 
   def initialize
     set_default_attributes
@@ -10,7 +10,7 @@ class Bill::AllSupporters
   def set_default_attributes
     self.short_title = 'All Supporters'
     self.chamber_size= 535
-    self.needed_cosponsor_count = 100
+    self.needed_cosponsor_count = TOTAL_NEEDED-Legislator.current_supporters.count
     self.congressional_session = 114
   end
 
