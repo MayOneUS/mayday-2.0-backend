24da17d3e3c25ea4d5491b29249a4a2e98f74db1
Rio Flores <rio517@gmail.com>
2015-05-07 13:28:08 -0400

    Add annotation

--- app/models/action.rb
+++ app/models/action.rb
@@ -2,11 +2,15 @@
 #
 # Table name: actions
 #
-#  id          :integer          not null, primary key
-#  person_id   :integer          not null
-#  activity_id :integer          not null
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
+#  id           :integer          not null, primary key
+#  person_id    :integer          not null
+#  activity_id  :integer          not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  utm_source   :string
+#  utm_medium   :string
+#  utm_campaign :string
+#  source_url   :string
 #
 
 class Action < ActiveRecord::Base
