1019720561d0ac4de8aa57d23cfaed6189a9d9ce
Rio Flores <rio517@gmail.com>
2015-06-12 17:54:09 -0400

    Sidekiq - always go inline in development

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,15 @@
+# config/initializers/sidekiq.rb
 if ENV['SIDEKIQ_TESTING'] == 'inline' || ENV['SIDEKIQ_TESTING'] == 'fake'
   require 'sidekiq/testing'
   if ENV['SIDEKIQ_TESTING'] == 'inline'
-    Sidekiq::Testing.inline!
+    Sidekiq::Testing.inline! # Perform Sidekiq jobs immediately
   elsif ENV['SIDEKIQ_TESTING'] == 'fake'
-    Sidekiq::Testing.fake!
+    Sidekiq::Testing.fake! # Ignore Sidekiq jobs
   end
+end
+
+
+if Rails.env.development?
+  require 'sidekiq/testing'
+  Sidekiq::Testing.inline! # Perform Sidekiq jobs immediately
 end
