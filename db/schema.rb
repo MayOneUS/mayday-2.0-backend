43b8e7006b4d231730741b148251be090ff4a529
Rio Flores <rio517@gmail.com>
2015-06-12 12:44:13 -0400

    Schema: forgot to commit a file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150609164951) do
+ActiveRecord::Schema.define(version: 20150611180719) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -99,6 +99,7 @@ ActiveRecord::Schema.define(version: 20150609164951) do
     t.datetime "updated_at",          null: false
     t.string   "call_type"
     t.string   "remote_origin_phone"
+    t.string   "campaign_ref"
   end
 
   add_index "ivr_calls", ["person_id"], name: "index_ivr_calls_on_person_id", using: :btree
