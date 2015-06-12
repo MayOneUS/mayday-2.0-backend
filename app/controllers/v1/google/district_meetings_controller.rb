cc14b1bb8e444fc2e98d4e7a5356d136b0f19d17
Rio Flores <rio517@gmail.com>
2015-06-12 18:18:36 -0400

    Updating google form mappings for new questions

--- app/controllers/v1/google/district_meetings_controller.rb
+++ app/controllers/v1/google/district_meetings_controller.rb
@@ -2,15 +2,18 @@ class V1::Google::DistrictMeetingsController < V1::Google::ApplicationController
   FORM_ID = '13Fvsp8EVE-2wgwM1GqeWQD9XXqr15U29HOv5GcoZH8A'
   FORM_FIELDS = %i[volunteer_tasks political_leaning congress_relationship]
 
+  # NOTE: values for these mappings must match the options in the google form.
   MAPPINGS = {
     email:                 'entry.273201208',
     first_name:            'entry.1567224396',
     last_name:             'entry.1155696049',
     phone:                 'entry.1049559909',
     zip:                   'entry.828776517',
-    volunteer_tasks:       'entry.493925815',
     political_leaning:     'entry.976801239',
     congress_relationship: 'entry.1981079718',
+    volunteer_letter:      'entry.129841063',
+    volunteer_attend:      'entry.395048103',
+    volunteer_call:        'entry.1368866945'
   }
 
 end
