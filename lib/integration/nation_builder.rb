af8d82656fd1a44566810210a0b14713cb74bca4
Rio Flores <rio517@gmail.com>
2015-06-17 13:13:13 -0400

    Add logging for nationbuilder pushes

--- lib/integration/nation_builder.rb
+++ lib/integration/nation_builder.rb
@@ -69,6 +69,7 @@ class Integration::NationBuilder
   def self.create_or_update_person(attributes:)
     rescue_oauth_errors do
       body = {'person': parse_person_attributes(attributes)}
+      Rails.logger.info "Pushing person to NationBuilder: #{body}"
       response = request_handler(endpoint_path: ENDPOINTS[:people], body: body, method: 'put')
       response['person']
     end
