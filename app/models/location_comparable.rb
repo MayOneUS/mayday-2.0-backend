05878451056a60eea3de31de68db119750a25a36
JT <jturino@gmail.com>
2016-03-03 14:52:39 -0500

    refactor LocationComparable

--- app/models/location_comparable.rb
+++ app/models/location_comparable.rb
@@ -1,44 +1,22 @@
-# == Schema Information
-#
-# Table name: locations
-#
-#  id            :integer          not null, primary key
-#  location_type :string
-#  address_1     :string
-#  address_2     :string
-#  city          :string
-#  state_id      :integer
-#  zip_code      :string
-#  person_id     :integer
-#  district_id   :integer
-#  created_at    :datetime         not null
-#  updated_at    :datetime         not null
-#
-
 class LocationComparable < Location
 
-  def merge(location)
-    if similar_to?(location)
-      base_values = {}
-    else
-      base_values = blank_attributes
+  def merge(other)
+    new_attributes = other.address_data
+    if similar_to?(other)
+      new_attributes.compact!
     end
-    assign_attributes(base_values.merge(location.address_data))
+    assign_attributes(new_attributes)
   end
 
   def similar_to?(location)
-    new_address = location.tap(&:fill_in_state).address_data
-    intersection = address_data.keys & new_address.keys
-    address_data.slice(*intersection) == new_address.slice(*intersection)
+    other_address = location.tap(&:fill_in_state).address_data
+    intersection = address_data.compact.keys & other_address.compact.keys
+    address_data.slice(*intersection) == other_address.slice(*intersection)
   end
 
   protected
 
   def address_data
-    attributes.symbolize_keys.slice(*ADDRESS_FIELDS).compact
-  end
-
-  def blank_attributes
-    ADDRESS_FIELDS.map{|k| [k, nil]}.to_h
+    attributes.symbolize_keys.slice(*ADDRESS_FIELDS)
   end
 end
