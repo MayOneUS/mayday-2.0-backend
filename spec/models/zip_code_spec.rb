e22063fd9fc5ae2778804bca355ce2a43b330d0b
JT <jturino@gmail.com>
2016-04-14 14:26:44 -0400

    add specs for single_district methods on ZipCode

--- spec/models/zip_code_spec.rb
+++ spec/models/zip_code_spec.rb
@@ -16,4 +16,52 @@
 require 'rails_helper'
 
 describe ZipCode do
+  describe "#single_district?" do
+    it "returns true if zip code has 1 district" do
+      zip = ZipCode.new
+      zip.districts << District.new
+
+      result = zip.single_district?
+
+      expect(result).to be true
+    end
+
+    it "returns false if zip code has no districts" do
+      zip = ZipCode.new
+
+      result = zip.single_district?
+
+      expect(result).to be false
+    end
+
+    it "returns false if zip code has multiple districts" do
+      zip = ZipCode.new
+      zip.districts << [District.new, District.new]
+
+      result = zip.single_district?
+
+      expect(result).to be false
+    end
+  end
+
+  describe "#single_district" do
+    it "returns district if zip code has 1 district" do
+      zip = ZipCode.new
+      district = District.new
+      zip.districts << district
+
+      result = zip.single_district
+
+      expect(result).to eq district
+    end
+
+    it "returns nil if zip code has multiple districts" do
+      zip = ZipCode.new
+      zip.districts << [District.new, District.new]
+
+      result = zip.single_district
+
+      expect(result).to be nil
+    end
+  end
 end
